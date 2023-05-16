7921a3e68b6e7ee2cc6acb464e5b496349953d94
sindhujamohan9 <sindhujamohan92@gmail.com>
2023-05-16 11:29:20 -0600

    Create forest_fire.py

---
+++ sparsification/forest_fire.py
@@ -0,0 +1,20 @@
+import networkit as nk
+
+G = nk.readGraph("../input/jazz.graph", nk.Format.METIS)
+G.indexEdges()
+G.numberOfNodes(), G.numberOfEdges()
+
+targetRatio = 0.2
+
+# Initialize the algorithm
+ffs = nk.sparsification.ForestFireScore(G, 0.6, 5.0)
+
+# Run
+ffs.run()
+
+# Get edge scores
+attributes = ffs.scores()
+for attribute in attributes[:5]:
+    print("{:.3f}".format(attribute))
+    
+  
